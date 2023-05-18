1971936b40b1840a096faff92786cfe7aa072ff1
Yassine Lafraouzi <mhtajek.sup@gmail.com>
2023-05-18 08:52:37 -0700

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main - A program that prints a line with puts function
- *Return : 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return : 0 (Success)
  */
 int main(void)
 {
