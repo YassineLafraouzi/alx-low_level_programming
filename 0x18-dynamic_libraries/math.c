4859af3e191000a3d5374d2dcb3470d2e26f2e85
Yassine Lafraouzi <mhtajek.sup@gmail.com>
2023-08-21 09:23:08 -0700

    updated

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,21 @@
+// operations.c
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0; // Gestion de la division par zéro
+    }
+}
+
